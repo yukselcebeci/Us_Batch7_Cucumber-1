8b28851a5c7cc764857cd7e4ce07a9a63cff7c16
ragipbaylar <ragipbaylar86@gmail.com>
2023-03-20 21:57:38 -0400

    Cucumber day 7 third commit

--- src/test/java/StepDefinitions/Hooks.java
+++ src/test/java/StepDefinitions/Hooks.java
@@ -1,22 +1,39 @@
 package StepDefinitions;
 
 import Utilities.BasicDriver;
-import io.cucumber.java.After;
-import io.cucumber.java.AfterStep;
-import io.cucumber.java.Before;
-import io.cucumber.java.BeforeStep;
+import io.cucumber.java.*;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+import java.io.File;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class Hooks {
 
     @Before // This method runs before every scenario
-    public void beforeScenario(){
+    public void beforeScenario() {
         System.out.println("Scenario has started");
     }
 
     @After // This method runs after every scenario
-    public void afterScenario(){
-        BasicDriver.quitDriver();
+    public void afterScenario(Scenario scenario) {
         System.out.println("Scenario has ended");
+        if (scenario.isFailed()){
+            TakesScreenshot takesScreenshot = (TakesScreenshot) BasicDriver.getDriver();
+            File file = takesScreenshot.getScreenshotAs(OutputType.FILE);
+            LocalDateTime timeOfError = LocalDateTime.now();
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMddyyyyHHmmss");
+            try {
+                FileUtils.copyFile(file,new File("src/test/java/ScreenShots/screenshot"
+                        +timeOfError.format(formatter)+".png"));
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+            BasicDriver.quitDriver();
     }
 
 //    @BeforeStep
@@ -28,7 +45,6 @@ public class Hooks {
 //    public void afterStep(){
 //        System.out.println("Step has ended");
 //    }
-
 
 
 }
