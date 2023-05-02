c14e1d66873efa9995f6c1d1caa3f4f4305e1266
ragipbaylar <ragipbaylar86@gmail.com>
2023-05-02 19:46:48 -0400

    Plug in is added for Jenkins report

--- src/test/java/Runners/_04_RunnerForGroups.java
+++ src/test/java/Runners/_04_RunnerForGroups.java
@@ -7,7 +7,7 @@ import io.cucumber.testng.CucumberOptions;
         tags = "@SmokeTest", // runs the scenarios that have this tag
         features = {"src/test/java/FeatureFiles"},
         glue = {"StepDefinitions"},
-        plugin = {"pretty", "html:target/cucumber", "json:target/cucumber.json"}
+        plugin = {"pretty", "html:target/site/cucumber-pretty", "json:target/cucumber/cucumber.json"}
 )
 public class _04_RunnerForGroups extends AbstractTestNGCucumberTests {
 }
